9e90264c5bebdf7d0a5dd94f8c2a1d7c34425024
subash3-git <nethajisubash3487@gmail.com>
2025-08-19 09:58:57 +0530

    Add files via upload

---
+++ Open cv/read.py
@@ -0,0 +1,9 @@
+import cv2
+import matplotlib.pyplot as plt
+img = cv2.imread("images.jpg")
+print(img) 
+
+cv2.rectangle(img,(50,50),(100,100),(255,9,0),2)
+
+plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+plt.show()
